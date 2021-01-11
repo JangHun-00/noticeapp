b050b84cd9226268d07a6c3f08d20f56f7646da3
JangHun-00 <janghun0056@gmail.com>
2021-01-11 18:46:21 +0900

    Add toolbar and empty menu bar

--- app/src/main/java/com/example/noticeapp/MainActivity.kt
+++ app/src/main/java/com/example/noticeapp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.noticeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // toolbar 연결
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val ab = supportActionBar!!
+        ab.setDisplayShowTitleEnabled(false)
+
+        // 내비게이션 뷰 (메뉴바)
+        menu_btn.setOnClickListener {
+            layout_drawer.openDrawer(GravityCompat.START)
+        }
+        naviView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // 내비게이션 메뉴 아이템 클릭시 수행
+        when (item.itemId){
+            R.id.board -> {}
+            R.id.scrap -> {}
+        }
+        layout_drawer.closeDrawers() // 내비게이션 뷰 닫기
+        return false
     }
 }
